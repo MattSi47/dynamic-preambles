77d33bcdfb60b2629073b5e3fa05f824d544c5fd
Matt Silverman <matt.silverman47@gmail.com>
2024-02-09 14:50:23 -0500

    updated script to include ' ,' between values

--- chirp_gen.py
+++ chirp_gen.py
@@ -11,13 +11,16 @@ Tpreamble = 40e-6
 N = round(Fs*Tpreamble)
 def chirp_fn(t, f0=f_start, f1=f_end, T=Tpreamble):
     theta = 2*math.pi*((f0-f1)/T)*t**2 + f0*T
-    return f"complex({np.float32(math.cos(theta))},{np.float32(math.sin(theta))})"
+    return f"complex({np.float32(math.cos(theta))},{np.float32(math.sin(theta))}),"
 
 print(f"N={N}")
 
 chirp_sig = "("
 for n in range(N):
     chirp_sig += chirp_fn(n*Ts)
+
+#remove last ","
+chirp_sig = chirp_sig[:-1]
 chirp_sig += ")"
 
 with open('chirp.txt', 'w') as f:
